0a723e340641440209a36e0dadbc6eb05e1f44cd
sankshit <sankshit.pandoh@matelabs.in>
2023-01-27 12:56:01 +0530

    Defined test scope

--- src/Components/TestLander.tsx
+++ src/Components/TestLander.tsx
@@ -15,7 +15,22 @@ export const TestLander = (props: any) => {
                 <h4>Pick one of the test from below:</h4>
                 <ul>
                     <li onClick={() => props.history.push("/singleCardTest")} className="test-list-item">Card List Tests</li>
-                    <li onClick={() => props.history.push("/virtualListTest")} className="test-list-item">Virtual List Tests</li>
+                    &nbsp; 
+                    <sup>
+                        Testing around: <br />
+                        &nbsp; - data fetching <br />
+                        &nbsp; - data rendering<br />
+                        &nbsp; - data filtering<br />
+                    </sup>
+                    <li onClick={() => props.history.push("/virtualListTest")} className="test-list-item">Form Tests</li>
+                    &nbsp; 
+                    <sup>
+                        Testing around: <br />
+                        &nbsp; - Form Validations <br />
+                        &nbsp; - Form Output<br />
+                        &nbsp; - Form Data Calculation<br />
+                        &nbsp; - Form Actions<br />
+                    </sup>
                 </ul>
 
             </CardContent>
