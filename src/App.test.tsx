04f6e4a8f948a3e87a681adb460eddd751dc45f1
sankshit <sankshit.pandoh@matelabs.in>
2023-01-27 13:00:23 +0530

    Basic react testing init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// the below case should fail
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
